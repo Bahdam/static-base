474766dbe01800b5c31591ef096db1691dd7c427
Adam Cole <adamcole123@hotmail.co.uk>
2023-09-10 20:27:47 +0100

    Github pages workflow

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
 	<div>
 		<h1>This is a template site</h1>
 		<p>This is the base site that we use to change the config and send to a client.</p>
-		<video src="/home-video.mp4/" alt="video for home" class="home-video"/>
 		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore laboriosam labore libero nisi quo cupiditate. Dolorum repellendus aliquid, autem cumque, sint necessitatibus, cupiditate eum explicabo modi nobis at voluptates! Quibusdam!</p>
 		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore laboriosam labore libero nisi quo cupiditate. Dolorum repellendus aliquid, autem cumque, sint necessitatibus, cupiditate eum explicabo modi nobis at voluptates! Quibusdam!</p>
 		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore laboriosam labore libero nisi quo cupiditate. Dolorum repellendus aliquid, autem cumque, sint necessitatibus, cupiditate eum explicabo modi nobis at voluptates! Quibusdam!</p>
