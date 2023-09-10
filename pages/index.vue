9a679386694466a1dc90bb86f06cf8785f5aa8d0
Adam Cole <adamcole123@hotmail.co.uk>
2023-09-10 21:19:36 +0100

    Got gif working

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<h1>This is a template site</h1>
-		<img src="/assets/tobi.gif"/>
+		<img src="tobi.gif"/>
 		<p>This is the base site that we use to change the config and send to a client.</p>
 		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore laboriosam labore libero nisi quo cupiditate. Dolorum repellendus aliquid, autem cumque, sint necessitatibus, cupiditate eum explicabo modi nobis at voluptates! Quibusdam!</p>
 		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore laboriosam labore libero nisi quo cupiditate. Dolorum repellendus aliquid, autem cumque, sint necessitatibus, cupiditate eum explicabo modi nobis at voluptates! Quibusdam!</p>
